e82bd9826f9d2f2df6903cd4610748431766578a
Aaron Adams <aaronadams.ca@gmail.com>
2024-07-28 12:36:09 +0200

    Initial reproduction

---
+++ README.md
@@ -0,0 +1,3 @@
+https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/270
+
+https://github.com/user-attachments/assets/bc6085c1-c7ef-4990-8cc0-ff48d7c589aa
---
+++ my-app/src/main.jsx
@@ -0,0 +1,10 @@
+import "./tailwind.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div className="py-16 px-8">Hello world</div>
+  </React.StrictMode>,
+);
