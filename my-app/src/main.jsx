988c6c7cafa6b4eaf452cd85620294929498c786
Aaron Adams <aaronadams.ca@gmail.com>
2024-09-21 18:25:52 +0000

    Update reproduction

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,15 @@
-import "./tailwind.css";
+import "#tailwind.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="py-16 px-8">Hello world</div>
+    <div
+      className="py-16 px-8 font-bold data-bounce:animate-bounce"
+      data-bounce
+    >
+      Hello world
+    </div>
   </React.StrictMode>,
 );
